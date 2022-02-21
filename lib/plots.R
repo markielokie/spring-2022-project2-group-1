79e66a41ebe458d6464404e7b37470dc82b2ec52
Marcus Loke <82932735+markielokie@users.noreply.github.com>
2022-02-21 11:45:18 -0500

    Change pheatmap labs angle

--- lib/plots.R
+++ lib/plots.R
@@ -163,5 +163,6 @@ plot.corr <- function(df, thres=0.7) {
   
   pheatmap(as.matrix(complaint_corr_filt), 
            main = "Correlation between Covid and Non-Covid Calls", 
-           fontsize = 12)
+           fontsize = 12,
+           angle_col = "0")
 }
